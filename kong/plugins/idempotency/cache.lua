731e8c5abbbd2e637a19637d7d5169c8cec24356
David Washington <davidwashington833@gmail.com>
2021-08-05 17:56:03 -0300

    Retry redis connection

--- kong/plugins/idempotency/cache.lua
+++ kong/plugins/idempotency/cache.lua
@@ -1,15 +1,25 @@
 local redis = require 'redis'
-local kong = kong
 local _M = {
   client = nil
 }
 
+function ping()
+  return _M.client:ping()
+end
+
 function _M.connection(conf)
   if _M.client then
-    return _M.client
+    local result = pcall(ping)
+
+    if result then
+      return _M.client
+    end
+
+    _M.client = nil
   end
 
-  _M.client = redis.connect(conf.redis_host, conf.redis_port)
+  client = redis.connect(conf.redis_host, conf.redis_port)
+  _M.client = client
 
   return _M.client
 end
